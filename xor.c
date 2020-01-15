1cfb0d24efc265a2037a40e54914309c962fcfa0
Nikita Ermakov <coffe92@gmail.com>
2020-01-15 12:05:35 +0300

    Do what was TODO.
    We do not remember exactly what we did. But, it is seems that it is
    working now? We guess?

--- xor.c
+++ xor.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define N 4
-#define M 4
+#define N 2
+#define M 2
 
 typedef struct
 {
@@ -22,8 +22,19 @@ mult_matrix(matrix *a, matrix *b)
     for (int j = 0; j < M; j++)
       for (int k = 0; k < M; k++)
       {
-        c->m[i][j] += a->m[i][k]*b->m[k][i]; 
+        c->m[i][j] += a->m[i][k]*b->m[k][j]; 
       }
+  return c;
+}
+
+void
+mult_matrix_s(matrix *a, float s)
+{
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < M; j++)
+    {
+      a->m[i][j] *= s;
+    }
 }
 
 void
@@ -59,6 +70,28 @@ read_vector(char *s)
   return f;
 }
 
+matrix*
+read_matrix(char *s)
+{
+  matrix *f = (matrix *)malloc(sizeof(matrix));
+  int i = 0, j = 0;
+  char *pt;
+  pt = strtok(s, ",");
+  while (pt)
+  {
+    f->m[i++][j] = atof(pt);
+    if (i >= N)
+    {
+      i = 0;
+      j++;
+    }
+    if (j >= M)
+      break;
+    pt = strtok(0, ",");
+  }
+  return f;
+}
+
 void
 print_matrix(matrix *x)
 {
@@ -72,11 +105,43 @@ print_matrix(matrix *x)
   }
 }
 
+matrix*
+read_matrix_from_file(const char *fname)
+{
+  FILE *f = fopen(fname, "r+");
+  if (!f)
+    return 0;
+  char buf[32] = {0};
+  fread(buf, 32, 1, f);
+  matrix *m = read_matrix(buf);
+  fclose(f);
+  return m;
+}
+
+void
+write_matrix_to_file(const char *fname, matrix *m)
+{
+  FILE *f = fopen(fname, "r+");
+  if (!f)
+    return;
+  char buf[32] = {0};
+  for (int i = 0; i < N; i++)
+    for(int j = 0; j < M; j++)
+    {
+      if (i == (N - 1) && j == (N - 1) )
+        fprintf(f, "%f", m->m[i][j]);
+      else
+        fprintf(f, "%f,", m->m[i][j]);
+    }
+  fputc('\n', f);
+  fclose(f);
+}
+
 int
 main(int argc, char **argv)
 {
-  const matrix b = {.m = {1.}};
-  const float eta = 1.;
+  matrix b = {.m = {0.}};
+  const float eta = 0.2;
   int learn = 0;
   matrix *x, *f0, *w, *f, *delta;
 
@@ -86,32 +151,36 @@ main(int argc, char **argv)
   
   if (argc == 3)
   {
-      learn = 1;
-      f0 = read_vector(argv[2]);
+    learn = 1;
+    f0 = read_vector(argv[2]);
   }
   x = read_vector(argv[1]);
 
   print_matrix(x);
 
-  /* TODO
-  FILE *file = fopen("weight", "r+");
   if (!f)
     return 0;
-  fread(&w, sizeof(w), 1, file);
+  w = read_matrix_from_file("weight");  
 
-  f = activation(x, w, b);
+  f = activation(x, w, &b);
   if (learn)
   {
-    delta = eta*(f0 - f)*x;
-    w += delta;
-    fseek(file, 0, SEEK_SET);
-    fwrite(&w, sizeof(w), 1, file);
+//    delta = eta*(f0 - f)*x;
+    add_matrix(f0, f, -1.);
+    mult_matrix_s(f0, eta);
+    delta = mult_matrix(f0, x);
+    add_matrix(w, delta, 1.);
+    write_matrix_to_file("weight", w);
   }
-  printf("answer = %f\nweight = %f\n", f, w);
-
-  fclose(file);
-  */
+  printf("answer:\n");
+  print_matrix(f);
+  printf("weights:\n");
+  print_matrix(w);
 
   free(x);
+  free(delta);
+  free(w);
+  free(f0);
+  free(f);
   return 0;
 }
